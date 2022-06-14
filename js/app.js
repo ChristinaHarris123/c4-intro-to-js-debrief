da844c90f6dc7d25ad513eba48ec78b2fa367729
Christina Harris <harrischristina4688@gmail.com>
2022-06-14 19:56:20 -0400

    I have made an edit to a file!

--- js/app.js
+++ js/app.js
@@ -8,7 +8,21 @@
 // If the value of myNumber is any other number print the value of myNumber to the console.
 // Reference Codecademy lessons: Introduction- Arithmetic Opertaors, Conditional Statements lessons 3, 4, 5,and 9
 
-//Write your code for GenBuzz below this line:
+//Write your code for GenBuzz below this line
+let myNumber = 15;
+
+if (typeof myNumber !== "number") {
+    console.log('This is not a number.');
+}else if (myNumber % 3 == 0 && myNumber % 5 == 0) {
+    console.log('GenBuzz');
+}else if (myNumber % 3 == 0) {
+    console.log('Gen');
+}else if (myNumber % 5 == 0) {
+    console.log('Buzz');
+}else {
+    console.log(myNumber);
+}
+    
 
 
 
@@ -17,7 +31,7 @@
 
 //2. E-COMMERCE ITEM LIST
 
-// Use a swtich statement to print the price of the each item in the store to the console.
+// Use a switch statement to print the price of the each item in the store to the console.
 // Ex. If the value of the variable is 'shoes' then, the statement "Shoes are $50" should be printed to the console.
 // Reference: Codecademy lesson 10
 // Items:
@@ -28,7 +42,26 @@
 // If the variable input is not an item in the store, then print 'Invalid Item' to the console.
 
 //Write your code for the E-Commerce item list below this line:
+let itemPrice = 'jeans'
 
+switch (itemPrice) {
+    case 'shoes':
+        //console.log('Shoes are $50.')
+        break;
+    case 'jeans':
+       // console.log('Jeans are $25.')
+        break;
+    case 'hats':
+       // console.log('Hats are $12.')
+        break;
+    case 'socks':
+       // console.log('Socks are $2.')
+        break;
+    default:
+       // console.log('Invalid Item')
+
+
+}
 
 
 
@@ -36,6 +69,10 @@
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 //Write your code below this line:
-
-
+function getRandomInt(min, max) {
+    min = Math.ceil(50);
+    max = Math.floor(100);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+//console.log(getRandomInt(50, 100));
 
